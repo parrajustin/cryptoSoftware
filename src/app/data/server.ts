1d47f4ba5d3873ae65bbd1e5d1a8942f7257e5c7
Unknown <alfred.cazares@gmail.com>
2017-12-01 15:25:20 -0700

    Worked on the User, admin, registered and guest classes again

---
+++ src/app/data/server.ts
@@ -0,0 +1,13 @@
+export class Server {
+    
+        public publicVar: number = 0;
+        public otherVar: string = "hi";
+        private test;
+    
+        constructor(
+        ) { }
+    
+        public func() {
+            // testasdf
+        }
+    }
