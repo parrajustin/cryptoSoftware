5d1794069560d60a2481d23dc4cee7f6154c367e
Freddy <alfred.cazares@gmail.com>
2017-11-30 20:19:33 -0700

    added empty classes to src\app\data

---
+++ src/app/data/administrator.ts
@@ -0,0 +1,16 @@
+
+
+// new update
+export class Administrator {
+    
+      public publicVar: number = 0;
+      public otherVar: string = "hi";
+      private test;
+        
+      constructor(
+      ) {}
+    
+      public func() {
+        // testasdf
+      }
+    }
